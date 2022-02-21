4646aaba8f8277e0531f6a2436bebbe322be2bf1
ozantekin <ardilozantekin@gmail.com>
2022-02-21 14:15:17 +0300

    RoundFloorCeil eklendi

---
+++ 07-RoundFloorCeil/app.js
@@ -0,0 +1,14 @@
+console.clear();
+
+const a=1.6;
+const b=1.2;
+// x.5 altını aşağıya yuvarlar
+console.log("round", Math.round(a)); 
+// x.5 ve üzerine yukarı yuvarlar
+console.log("round", Math.round(b));
+
+const c=2.5;
+// hep aşağıya yuvarlar
+console.log("floor", Math.floor(c));
+// hep yukarı yuvarlar
+console.log("ceil", Math.ceil(c));
